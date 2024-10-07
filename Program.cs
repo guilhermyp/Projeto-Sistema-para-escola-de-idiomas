2ba5593000e6468bb8a93fae717783c870842f59
Rafael Zacharkim <rafaelzacharkim@outlook.com>
2024-10-07 02:11:29 -0300

    Adiciona endpoints para gerenciamento de alunos e matérias: implementa operações GET, POST e DELETE; configura o DbContext para SQLite e inicializa dados de matérias.

--- Program.cs
+++ Program.cs
@@ -11,4 +11,43 @@ app.MapGet("/materias", async ([FromServices] AppDbContext db) =>
     return await db.Materias.ToListAsync();
 });
 
+app.MapPost("/alunos", async ([FromServices] AppDbContext db, [FromBody] Aluno novoAluno) =>
+{
+    
+    var alunoExistente = await db.Alunos.FindAsync(novoAluno.Matricula);
+    if (alunoExistente != null)
+    {
+        return Results.BadRequest("Aluno já existe.");
+    }
+
+   
+    db.Alunos.Add(novoAluno);
+
+   
+    await db.SaveChangesAsync();
+
+    
+    return Results.Created($"/alunos/{novoAluno.Matricula}", novoAluno);
+});
+
+app.MapDelete("/alunos/{matricula:int}", async ([FromServices] AppDbContext db, int matricula) =>
+{
+    var aluno = await db.Alunos.FindAsync(matricula);
+    if (aluno is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Alunos.Remove(aluno); 
+    await db.SaveChangesAsync(); 
+
+    return Results.NoContent(); 
+});
+
+app.MapGet("/alunos", async ([FromServices] AppDbContext db) =>
+{
+    return await db.Alunos.ToListAsync(); // Retorna a lista de alunos
+});
+
+
 app.Run();
